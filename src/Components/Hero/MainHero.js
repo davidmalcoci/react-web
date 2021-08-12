76d4c1467fe1da45f033f92f10b9434690b24b48
David <davidmalcoci@live.it>
2021-08-12 16:45:43 +0200

    fixed button

--- src/Components/Hero/MainHero.js
+++ src/Components/Hero/MainHero.js
@@ -1,4 +1,6 @@
+import About from '../Pages/About/About';
 import './MainHero.css';
+import { Link } from 'react-router-dom';
 
 const MainHero = () => {
     return (
@@ -9,7 +11,7 @@ const MainHero = () => {
     <div className="col-sm-12 col-md-6">
       <h1 className="text-light">Coding on steroids</h1>
       <p className="text-white-60">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum in rutrum eros, in consequat nulla. Donec ac porttitor tortor.</p>
-      <a role="button" href='/about' className="btn btn-dark text-light">Give it a spin</a>
+      <Link to='/about' className="btn btn-dark text-light">Give it a spin</Link>
       </div>
     </div>
   </div>
