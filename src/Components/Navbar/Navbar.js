3cbd95a2e14927f995b53128abcd068415d547f4
David <davidmalcoci@live.it>
2021-08-12 13:54:00 +0200

    about start

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavItem from './NavItem';
 import './Navbar.css';
 import About from '../Pages/About';
@@ -12,7 +12,7 @@ const Navbar = () => {
         <div>
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <a className="navbar-brand" href=''>Inguzo</a>
+    <a className="navbar-brand" href='index'>Inguzo</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
@@ -20,20 +20,14 @@ const Navbar = () => {
       <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
         <NavItem name='Home' link='index'/>
         <NavItem name='About' link='/about'/>
-        <NavItem name='Contact' link=''/>
-        <NavItem name='Real Time+' link=''/>
-
-        
+        <NavItem name='Contact' link='/contact'/>
+        <NavItem name='Real Time+' link='/monitor'/>
       </ul>
     </div>
   </div>
 </nav>
 
 <Route path="/about" component={About}/>
-          <Route path="">
-          <index/>
-          </Route>
-
 </div>
 </Switch>
 </Router>
--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -1,9 +1,17 @@
 import './About.css';
+import AboutDetails from './AboutDetails';
 
 const About = () => {
     return (
         <div>
-            <h1 className="text-center">Hello World!</h1>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 w-100 main">
+                    <h1 className="text-center mt-5 pb-5">About Inguzo</h1>
+                    <AboutDetails/>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
